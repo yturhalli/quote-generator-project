c1932bbedab9c671ff1f347a38c270480bb0bea7
yusuf-testinvite <yusuf@testinvite.com>
2022-08-20 10:49:36 +0300

    last files

---
+++ script.js
@@ -0,0 +1,60 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteAuthor = document.getElementById('author');
+const quoteText = document.getElementById('quote');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+async function getQuoteFromApi() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+
+    try {
+        showLoadingSpinner();
+        // const res = await fetch(apiURL);
+        const res = await fetch(proxyUrl + apiUrl)
+        data = await res.json();
+
+        if (quote.quoteAuthor === '') {
+            quoteAuthor.innerText = 'Unknown';
+        } else {
+            quoteAuthor.innerText = data.quoteAuthor;
+        }
+
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.textContent = data.quoteText;
+        removeLoadingSpinner();
+    } catch (error) {
+        getQuoteFromApi();
+    }
+}
+
+//Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${quoteAuthor.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', getQuoteFromApi);
+twitterBtn.addEventListener('click', tweetQuote)
+//On Load
+getQuoteFromApi();
+
